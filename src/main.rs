bb4f66a6ed72196dd1cbbdfcf865a0ca5b9cd40b
genichiro22 <scepterjw@yahoo.co.jp>
2023-10-03 22:45:18 +0900

    updated main for the first time

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,111 @@
 fn main() {
     println!("Hello, world!");
+    let mut player1 = Player {
+        life: 20,
+        hand: Domain::new("HAND"),
+        library: Domain::new("LIBRARY"),
+        battlefield: Domain::new("BATTLEFIELD")
+    };
+    let mut player2 = Player {
+        life: 30,
+        hand: Domain::new("HAND"),
+        library: Domain::new("LIBRARY"),
+        battlefield: Domain::new("BATTLEFIELD")
+    };
+    let mut game = Game {
+        status: Status {
+            turn: 0,
+            active_player: player1.clone(),
+            next_player: player2.clone(),
+            phase: Phase {
+                name: "BOOT",
+                step_belonged: Step {
+                    name: "BOOT"
+                }
+            }
+        },
+        board: Board {
+            turn: 0,
+            player: [player1.clone(), player2]
+        }
+    };
+    println!("{:?}",game.board.player[0].clone());
+    player1.life = 21;
+    println!("{:?}",game.board.player[0].clone());
+    player1.life = 21;
+    
 }
 
 #[derive(Debug)]
-struct Game {
-    status: Status,
-    board: Board,
+struct Game<'a> {
+    status: Status<'a>,
+    board: Board<'a>,
 }
 
 #[derive(Debug, Clone)]
-struct Board {
+struct Board<'a> {
     turn: usize,
-    player: [Player; 2],
+    player: [Player<'a>; 2],
 }
 
 #[derive(Debug, Clone)]
-struct Player {
-    user_id: usize,
+struct Player<'a> {
     life: usize,
-    hand: Domain,
-    library: Domain,
-    graveyard: Domain,
-    battlefield: Domain,
-    waiting: Domain,
+    hand: Domain<'a>,
+    library: Domain<'a>,
+    // graveyard: Domain,
+    battlefield: Domain<'a>,
+    // waiting: Domain,
+}
+
+trait Agent {
+    fn play_card(&self);
+    fn attack(&self);
 }
 
 #[derive(Debug, Clone)]
-struct Domain {
-    name: String,
+struct Domain<'a> {
+    name: &'a str,
     cards: Vec<Card>,
 }
 
-#[derive(Debug, Clone)]
+impl<'a> Domain<'a> {
+    fn new(name: &'a str) -> Self {
+        Domain {
+            name,
+            cards: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Card {
     id: usize,
     oracle: Oracle,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct Oracle {
     id: usize,
-    name: String,
+    // name: String,
     cost: usize,
 }
 
 #[derive(Debug, Clone)]
-struct Status {
-    id: usize,
+struct Status<'a> {
     turn: usize,
-    active_player: Player,
-    next_player: Player,
-    phase: Phase,
+    active_player: Player<'a>,
+    next_player: Player<'a>,
+    phase: Phase<'a>,
 }
 
-#[derive(Debug, Clone)]
-struct Phase {
-    name: String,
-    step_belonged: Step,
+#[derive(Debug, Clone, Copy)]
+struct Phase<'a> {
+    name: &'a str,
+    step_belonged: Step<'a>,
 }
 
-#[derive(Debug, Clone)]
-struct Step {
-    name: String,
+#[derive(Debug, Clone, Copy)]
+struct Step<'a> {
+    name: &'a str,
 }
